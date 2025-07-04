f5460c01bd400f8db4087241dce3d36ef4358d1a
Davy <davykanningstudent@gmail.com>
2025-07-04 11:41:40 +0200

    bump?

--- dAIOFisher/src/main/java/main/dAIOFisher.java
+++ dAIOFisher/src/main/java/main/dAIOFisher.java
@@ -37,11 +37,11 @@ import javax.imageio.ImageIO;
         name = "dAIOFisher",
         description = "AIO Fisher that fishes, banks and/or drops to get those gains!",
         skillCategory = SkillCategory.FISHING,
-        version = 1.97,
+        version = 1.98,
         author = "JustDavyy"
 )
 public class dAIOFisher extends Script {
-    public static String scriptVersion = "1.97";
+    public static String scriptVersion = "1.98";
     public static boolean setupDone = false;
     public static boolean usingBarrel = false;
     private static final java.awt.Font ARIEL = java.awt.Font.getFont("Ariel");
