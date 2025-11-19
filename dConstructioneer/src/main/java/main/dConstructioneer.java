cdce2f9cc205ba914a775f48b49f5d22231c0d80
Davy <davykanningstudent@gmail.com>
2025-11-19 21:40:50 +0100

    initial release

---
+++ dConstructioneer/src/main/java/main/dConstructioneer.java
@@ -0,0 +1,16 @@
+                    package main;
+
+                    import com.osmb.api.script.ScriptDefinition;
+import com.osmb.api.script.SkillCategory;
+
+                    @ScriptDefinition(
+        name = "dConstructioneer",
+        description = "Trains construction quickly by making hull parts or repair kits for the Sailing skill",
+        skillCategory = SkillCategory.CONSTRUCTION,
+        version = 1.0,
+        author = "JustDavyy"
+)
+                    public final class dConstructioneer {
+                        private dConstructioneer() {}
+                    }
+                    
