88fa002c87ca4351a591b867045523fb9f82e0b4
Davy <davykanningstudent@gmail.com>
2025-11-26 14:11:37 +0100

    AmethystCutter 1.0

---
+++ dAmethystCutter/src/main/java/main/dAmethystCutter.java
@@ -0,0 +1,16 @@
+                    package main;
+
+                    import com.osmb.api.script.ScriptDefinition;
+import com.osmb.api.script.SkillCategory;
+
+                    @ScriptDefinition(
+        name = "dAmethystCutter",
+        description = "Crafts your amethyst into useful resources",
+        skillCategory = SkillCategory.CRAFTING,
+        version = 1.0,
+        author = "JustDavyy"
+)
+                    public final class dAmethystCutter {
+                        private dAmethystCutter() {}
+                    }
+                    
