2555a5b02faf82bb2a8f9a7ef38964b8a95c0228
Davy <davykanningstudent@gmail.com>
2025-12-20 09:14:10 +0100

    2.7 - Forgotten brew support

--- dGemstoneCrabber/src/main/java/main/dGemstoneCrabber.java
+++ dGemstoneCrabber/src/main/java/main/dGemstoneCrabber.java
@@ -7,7 +7,7 @@ import com.osmb.api.script.SkillCategory;
         name = "dGemstoneCrabber",
         description = "Trains combat by hunting the gem stone crab",
         skillCategory = SkillCategory.COMBAT,
-        version = 2.6,
+        version = 2.7,
         author = "JustDavyy"
 )
                     public final class dGemstoneCrabber {
