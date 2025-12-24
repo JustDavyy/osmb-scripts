d886abee226d92f7067652dc939635d49b26041c
Davy <davykanningstudent@gmail.com>
2025-12-24 14:11:49 +0100

    2.4

--- dFossilWCer/src/main/java/main/dFossilWCer.java
+++ dFossilWCer/src/main/java/main/dFossilWCer.java
@@ -5,9 +5,9 @@ import com.osmb.api.script.SkillCategory;
 
                     @ScriptDefinition(
         name = "dFossilWCer",
-        description = "Cuts and drops/banks Teak or Mahogany logs on Fossil Island",
+        description = "Cuts and drops/banks hardwood trees on Fossil Island",
         skillCategory = SkillCategory.WOODCUTTING,
-        version = 2.2,
+        version = 2.4,
         author = "JustDavyy"
 )
                     public final class dFossilWCer {
