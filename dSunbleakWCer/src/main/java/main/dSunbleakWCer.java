704c76ef1623f3b72c3d427613ab63829178584a
Davy <davykanningstudent@gmail.com>
2025-12-31 15:00:53 +0100

    1.0

---
+++ dSunbleakWCer/src/main/java/main/dSunbleakWCer.java
@@ -0,0 +1,16 @@
+                    package main;
+
+                    import com.osmb.api.script.ScriptDefinition;
+import com.osmb.api.script.SkillCategory;
+
+                    @ScriptDefinition(
+        name = "dSunbleakWCer",
+        description = "Chops and optionally banks Ironwood logs on Sunbleak island",
+        skillCategory = SkillCategory.WOODCUTTING,
+        version = 1.0,
+        author = "JustDavyy"
+)
+                    public final class dSunbleakWCer {
+                        private dSunbleakWCer() {}
+                    }
+                    
