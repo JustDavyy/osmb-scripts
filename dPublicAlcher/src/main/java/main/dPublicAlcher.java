91c57595fca1f231ca5a87acb0019e04a6a649cb
Davy <davykanningstudent@gmail.com>
2026-01-14 12:21:43 +0100

    alcher 2.5

--- dPublicAlcher/src/main/java/main/dPublicAlcher.java
+++ dPublicAlcher/src/main/java/main/dPublicAlcher.java
@@ -6,7 +6,11 @@ import com.osmb.api.script.SkillCategory;
 
                     @ScriptDefinition(
         name = "dPublic Alcher",
-        description = "Alchs items (both high & low)
+        description = "Alchs items using high or low alch until out of items or runes.",
+        skillCategory = SkillCategory.MAGIC,
+        version = 2.5,
+        author = "JustDavyy"
+)
                     public final class dPublicAlcher {
                         private dPublicAlcher() {}
                     }
