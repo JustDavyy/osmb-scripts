497cea88245755e690d6b9bee463a1d431e5675f
Davy <davykanningstudent@gmail.com>
2026-01-21 20:36:39 +0100

    2.0

--- dSawmillRunner/src/main/java/main/dSawmillRunner.java
+++ dSawmillRunner/src/main/java/main/dSawmillRunner.java
@@ -7,7 +7,7 @@ import com.osmb.api.script.SkillCategory;
         name = "dSawmillRunner",
         description = "Creates planks of your choice at multiple sawmills",
         skillCategory = SkillCategory.CONSTRUCTION,
-        version = 1.9,
+        version = 2.0,
         author = "JustDavyy"
 )
                     public final class dSawmillRunner {
