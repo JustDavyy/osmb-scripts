b2d84e8aa5aea693fe10bc856ba4646f75e80d45
Davy <davykanningstudent@gmail.com>
2026-02-04 20:58:07 +0100

    2.7

--- dWyrmAgility/src/main/java/main/dWyrmAgility.java
+++ dWyrmAgility/src/main/java/main/dWyrmAgility.java
@@ -6,7 +6,7 @@ import com.osmb.api.script.SkillCategory;
                     @ScriptDefinition(
         name = "dWyrmAgility",
         author = "JustDavyy",
-        version = 2.6,
+        version = 2.7,
         description = "Does the Wyrm basic or advanced agility course.",
         skillCategory = SkillCategory.AGILITY
 )
