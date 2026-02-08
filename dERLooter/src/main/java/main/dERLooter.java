099c9c21a173ebdb691786bd118b3a8e6c4233e4
Davy <davykanningstudent@gmail.com>
2026-02-08 12:37:55 +0100

    1.7, now public

---
+++ dERLooter/src/main/java/main/dERLooter.java
@@ -0,0 +1,16 @@
+                    package main;
+
+                    import com.osmb.api.script.ScriptDefinition;
+import com.osmb.api.script.SkillCategory;
+
+                    @ScriptDefinition(
+        name = "dERLooter",
+        description = "Loots and banks eclipse reds in the hunter guild",
+        skillCategory = SkillCategory.OTHER,
+        version = 1.7,
+        author = "JustDavyy"
+)
+                    public final class dERLooter {
+                        private dERLooter() {}
+                    }
+                    
