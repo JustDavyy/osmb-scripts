6f3794c7e6ee4074cc04d0f35e8cc128143389a8
Davy <davykanningstudent@gmail.com>
2026-02-10 23:34:09 +0100

    Initial release

---
+++ dMLMv2/src/main/java/main/dMLMv2.java
@@ -0,0 +1,16 @@
+                    package main;
+
+                    import com.osmb.api.script.ScriptDefinition;
+import com.osmb.api.script.SkillCategory;
+
+                    @ScriptDefinition(
+    name = "dMLMv2",
+    threadUrl = "https://wiki.osmb.co.uk/article/dmlmv2",
+    skillCategory = SkillCategory.MINING,
+    version = 1.0,
+    author = "JustDavyy"
+)
+                    public final class dMLMv2 {
+                        private dMLMv2() {}
+                    }
+                    
