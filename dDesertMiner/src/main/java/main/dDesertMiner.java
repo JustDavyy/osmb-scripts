ddb81b4ba25e64ada39800a2f47c051d94101e79
Davy <davykanningstudent@gmail.com>
2026-03-06 16:06:42 +0100

    dDesertMiner 1.0

---
+++ dDesertMiner/src/main/java/main/dDesertMiner.java
@@ -0,0 +1,16 @@
+                    package main;
+
+                    import com.osmb.api.script.ScriptDefinition;
+import com.osmb.api.script.SkillCategory;
+
+                    @ScriptDefinition(
+name = "dDesertMiner",
+threadUrl = "https://wiki.osmb.co.uk/article/ddesertminer",
+        skillCategory = SkillCategory.MINING,
+        version = 1.0,
+        author = "JustDavyy"
+)
+                    public final class dDesertMiner {
+                        private dDesertMiner() {}
+                    }
+                    
